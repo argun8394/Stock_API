ae01b8078ca5f12341afe829a46c478c3c490d3a
argun8394 <argundoganali@gmail.com>
2024-06-04 17:14:09 +0300

    user_controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,2 +1,65 @@
 "use strict"
 /* -------------------------------------------------- */
+
+const User = require('../models/user')
+
+module.exports = {
+
+    list: async (req, res) => {
+
+        const data = await res.getModelList(User)
+
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(User),
+            data
+        })
+    },
+
+    create: async (req, res) => {
+
+        // Disallow setting admin/staff: 
+        req.body.is_staff = false
+        req.body.is_superadmin = false
+
+        const data = await User.create(req.body)
+
+        res.status(201).send({
+            error: false,
+            data
+        })
+    },
+
+    read: async (req, res) => {
+
+        const data = await User.findOne({ _id: req.params.id })
+
+        res.status(200).send({
+            error: false,
+            data
+        })
+    },
+
+    update: async (req, res) => {
+
+        const data = await User.updateOne({ _id: req.params.id }, req.body, { runValidators: true })
+
+        res.status(202).send({
+            error: false,
+            data,
+            new: await User.findOne({ _id: req.params.id })
+        })
+    },
+
+
+    delete: async (req, res) => {
+
+        const data = await User.deleteOne({ _id: req.params.id })
+
+        res.status(data.deletedCount ? 204 : 404).send({
+            error: !data.deletedCount,
+            data
+        })
+    },
+
+}
