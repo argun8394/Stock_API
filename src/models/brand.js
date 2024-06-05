a42c9f040d3875802fb03bd7b4636745ffdbd13e
argun8394 <argundoganali@gmail.com>
2024-06-05 12:27:33 +0300

    brand_model

--- src/models/brand.js
+++ src/models/brand.js
@@ -1,4 +1,34 @@
 "use strict"
 /* --------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
+/* ------------------------------------------------------- *
+{
+    "name": "Brand 1"
+}
 /* ------------------------------------------------------- */
+// Brand Model:
+
+const BrandSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+
+    image: { // URL
+        type: String,
+        trim: true
+    }
+
+}, { collection: 'brands', timestamps: true })
+
+/* ------------------------------------------------------- */
+
+BrandSchema.pre('init', function (data) {
+    data.id = data._id
+    data.createds = data.createdAt.toLocaleDateString('tr-tr')
+})
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Brand', BrandSchema)
