a33ac627c4c0f854cbb112d8c10526c8a2dc2a9d
argun8394 <argundoganali@gmail.com>
2024-06-05 12:28:12 +0300

    product_model

--- src/models/product.js
+++ src/models/product.js
@@ -1,4 +1,48 @@
 "use strict"
 /* ----------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
+
+/* ------------------------------------------------------- *
+{
+    "category_id": "65343222b67e9681f937f203",
+    "brand_id": "65343222b67e9681f937f107",
+    "name": "Product 1"
+}
 /* ------------------------------------------------------- */
+// Product Model:
+
+const ProductSchema = new mongoose.Schema({
+
+    category_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+
+    brand_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Brand',
+        required: true
+    },
+
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    stock: {
+        type: Number,
+        default: 0
+    }
+
+}, { collection: 'products', timestamps: true })
+
+/* ------------------------------------------------------- */
+
+ProductSchema.pre('init', function (data) {
+    data.id = data._id
+    data.createds = data.createdAt.toLocaleDateString('tr-tr')
+})
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Product', ProductSchema)
