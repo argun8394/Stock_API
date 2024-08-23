9f605f8408e1dc9ddcf96cd67a2f90474e16cf3c
argun8394 <argundoganali@gmail.com>
2024-08-23 15:30:43 +0300

    purchase_controller

--- src/controllers/purchase.js
+++ src/controllers/purchase.js
@@ -1,2 +1,66 @@
 "use strict"
 /* --------------------------------------------------- */
+
+// Purchase Controller:
+
+const Purchase = require('../models/purchase')
+
+module.exports = {
+
+    list: async (req, res) => {
+
+        const data = await res.getModelList(Purchase, {}, ['firm_id', 'brand_id', 'product_id'])
+
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(Purchase),
+            data
+        })
+
+        // res.status(200).send(data)
+    },
+
+    create: async (req, res) => {
+
+        // Auto add user_id to req.body:
+        req.body.user_id = req.user?._id
+
+        const data = await Purchase.create(req.body)
+
+        res.status(201).send({
+            error: false,
+            data
+        })
+    },
+
+    read: async (req, res) => {
+
+        const data = await Purchase.findOne({ _id: req.params.id }).populate(['firm_id', 'brand_id', 'product_id'])
+
+        res.status(200).send({
+            error: false,
+            data
+        })
+    },
+
+    update: async (req, res) => {
+
+        const data = await Purchase.updateOne({ _id: req.params.id }, req.body, { runValidators: true })
+
+        res.status(202).send({
+            error: false,
+            data,
+            new: await Purchase.findOne({ _id: req.params.id })
+        })
+    },
+
+    delete: async (req, res) => {
+
+        const data = await Purchase.deleteOne({ _id: req.params.id })
+
+        res.status(data.deletedCount ? 204 : 404).send({
+            error: !data.deletedCount,
+            data
+        })
+    },
+}
