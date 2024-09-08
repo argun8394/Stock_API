c03cecd46c867cc40869ff0d58c59a60a32c5003
argun8394 <argundoganali@gmail.com>
2024-09-09 01:23:34 +0300

    update auth routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,7 +9,7 @@ const auth = require('../controllers/auth')
 // URL: /auth
 
 router.post('/login', auth.login) // SimpleToken & JWT
-// router.post('/refresh', auth.refresh) // JWT Refresh
+router.post('/refresh', auth.refresh) // JWT Refresh
 router.get('/logout', auth.logout) // SimpleToken Logout
 router.post('/logout', auth.logout) // SimpleToken Logout
 
