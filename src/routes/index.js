12d9709cf2be9c31fedfb948dec09bfd388581aa
argun8394 <argundoganali@gmail.com>
2024-09-10 23:29:09 +0300

    update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,11 +8,14 @@ const router = require('express').Router()
 
 // // auth:
 router.use('/auth', require('./auth'))
+
+// call user.create for /account/register:
+const { create: userCreate } = require('../controllers/user')
+router.post('/account/register', userCreate)
 // user:
 router.use('/users', require('./user'))
 // // token:
 router.use('/tokens', require('./token'))
-
 
 // brand:
 router.use('/brands', require('./brand'))
